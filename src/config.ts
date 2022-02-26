21cd51b362ec94488fa16ef4cc801d322b1d402b
Federico <federico.dayan@gmail.com>
2022-02-26 14:36:16 -0800

    checking archive with github api

---
+++ src/config.ts
@@ -0,0 +1,15 @@
+import * as core from '@actions/core'
+import {context} from '@actions/github'
+
+export const artifactName = core.getInput('artifactName', {required: true})
+
+export const githubToken = core.getInput('GITHUB_TOKEN', {required: true})
+
+export const repo = buildRepo()
+
+function buildRepo(): {repo: string; owner: string} {
+  return {
+    owner: context.repo.owner,
+    repo: core.getInput('repo', {required: false}) || context.repo.repo
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,38 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import {artifactName, githubToken, repo} from './config'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    // const ms: string = core.getInput('milliseconds')
+    // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // core.debug(new Date().toTimeString())
+    // await wait(parseInt(ms, 10))
+    // core.debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString())
+    // core.setOutput('time', new Date().toTimeString())
+
+    const octokit = github.getOctokit(githubToken)
+    const artifacts = await octokit.request(
+      `GET /repos/${repo.owner}/${repo.repo}/actions/artifacts`,
+      {
+        owner: repo.owner,
+        repo: repo.repo
+      }
+    )
+
+    core.debug(artifacts.data)
+
+    if (artifacts.data && artifacts.data.artifacts) {
+      const artifactsFound = artifacts.data.artifacts.filter(
+        (a: {name: string; expired: boolean}) =>
+          a.name.match(artifactName) && !a.expired
+      )
+
+      core.setOutput('artifacts_found_length', artifactsFound.length)
+      core.setOutput('artifacts_found', artifactsFound.length > 0)
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
